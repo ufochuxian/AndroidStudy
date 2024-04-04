a386bf3f3d4263575a7a5a554db3f24ab04eae7d
eric <853409335@qq.com>
2024-04-04 15:11:21 +0800

    feaq:解决变黄的问题

---
+++ app/src/main/java/com/eric/kotlin/KotlinTest.java
@@ -0,0 +1,9 @@
+package com.eric.kotlin;
+
+/**
+ * @Author: chen
+ * @datetime: 2024/4/4
+ * @desc:
+ */
+class KotlinTest {
+}
--- app/src/main/java/com/eric/thread/ThreadTest.kt
+++ app/src/main/java/com/eric/thread/ThreadTest.kt
@@ -1,4 +1,4 @@
-package com.eric.generic.thread
+package com.eric.thread
 
 /**
 
