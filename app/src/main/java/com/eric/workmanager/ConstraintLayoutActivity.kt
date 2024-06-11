dce5e6eac704cc1e6eb747f99ff63b38de466e2d
陈建祥 <853409335@qq.com>
2024-06-11 11:43:38 +0800

    feaq:constraintLayout的goneMargin类似属性的用法

---
+++ app/src/main/java/com/eric/workmanager/ConstraintLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.eric.workmanager
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintLayoutActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+
+    }
+}
