4a1f4f2dd4b4c8284c6da12b228970cdf755b6cf
eric <853409335@qq.com>
2024-07-19 00:06:11 +0800

    feaq:测试代码使用drawable的例子

--- app/src/main/java/com/eric/androidstudy/SecondFragment.kt
+++ app/src/main/java/com/eric/androidstudy/SecondFragment.kt
@@ -1,13 +1,20 @@
 package com.eric.androidstudy
 
+import android.R
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.eric.androidstudy.R
+import com.blankj.utilcode.util.ConvertUtils.dp2px
 import com.eric.androidstudy.databinding.FragmentSecondBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -26,16 +33,44 @@ class SecondFragment : Fragment() {
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
 
+        shapeByCode()
+
+        setGradientDrawable()
+
+        return binding.root
+    }
+
+    private fun setGradientDrawable() {
+        // 创建一个 GradientDrawable
+        val gradientDrawable = GradientDrawable()
+        // 设置形状为矩形
+        gradientDrawable.shape = GradientDrawable.RECTANGLE
+        // 设置背景颜色，例如蓝色
+        gradientDrawable.setColor(Color.BLUE)
+        // 将 GradientDrawable 设置为某个视图的背景
+        binding.gradientDrawableBtn.background = gradientDrawable
+    }
+
+    private fun shapeByCode() {
+        val roundIn = dp2px(30f).toFloat()
+        val externalRounds =
+            floatArrayOf(roundIn, roundIn, roundIn, roundIn, roundIn, roundIn, roundIn, roundIn)
+        val outlineRound =
+            floatArrayOf(roundIn, roundIn, roundIn, roundIn, roundIn, roundIn, roundIn, roundIn)
+        val reactFValue = dp2px(2f).toFloat()
+        val rectF = RectF(reactFValue, reactFValue, reactFValue, reactFValue)
+        val drawable1 = ShapeDrawable(RoundRectShape(externalRounds, rectF, outlineRound))
+        drawable1.paint.color = Color.RED
+        binding.shapeCode.background = drawable1
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+//        binding.buttonSecond.setOnClickListener {
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+//        }
     }
 
     override fun onDestroyView() {
