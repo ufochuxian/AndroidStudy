30fd9e26ca7d61eeeec59e7d1c3a22bb9922bacd
陈建祥 <853409335@qq.com>
2024-08-12 23:59:13 +0800

    feaq:关于flow使用的一些用法和概念

--- app/src/main/java/com/eric/kotlin/corotinue/flow/FlowGuide.kt
+++ app/src/main/java/com/eric/kotlin/corotinue/flow/FlowGuide.kt
@@ -2,13 +2,16 @@ package com.eric.kotlin.corotinue.flow
 
 import android.app.Activity
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 
 
 private suspend fun Activity.flow() {
@@ -19,4 +22,37 @@ private suspend fun Activity.flow() {
     }.map { it ->
         println(it * 2)
     }.collect()
+}
+
+fun main() {
+    runBlocking {
+        val messageModelFlow = flow {
+            arrayOf(
+                MessageModel(ModelType.ASR, "asr"), MessageModel(ModelType.TTL, "ttl"),
+                MessageModel(ModelType.JLGL, "JLGL")
+            ).forEach {
+                emit(it)
+            }
+        }
+
+        delay(3000)
+
+        //flow是一个冷流，有观察者，才会触发生产发射，并且数据对于 每个观察者，都是独立的一份，不共享，并且没有生命周期的概念，发射完成就结束了.并且数据不会重置。并且没有缓存数据的概念
+        //这些都是区别于LiveData
+        messageModelFlow.collect {
+            println(it.content)
+        }
+
+        messageModelFlow.map {
+            it.content.plus("_super")
+        }.collect {
+            println(it)
+        }
+    }
+}
+
+data class MessageModel(val type: ModelType,val content : String)
+
+enum class ModelType {
+    ASR,TTL,JLGL
 }
--- app/src/main/java/com/eric/kotlin/corotinue/flow/MutableStateFlow.kt
+++ app/src/main/java/com/eric/kotlin/corotinue/flow/MutableStateFlow.kt
@@ -40,6 +40,11 @@ class MutableStateViewModel : ViewModel() {
  * Collector 2:count:2,name:MutableStateFlow
  *
  *
+ * 总结：StateFlow等同于一个特殊的“SharedFlow”,只有一个应答者，指定只缓存一个值。并且采用CompareGetAndSet，对新旧发射的值进行对比，如果新旧发射的值一样，就不会触发消费方进行消费啦
+ * 适用于UI状态的更新，永远需要保持ui最新状态的一些场景，因为没有生命周期感应能力，需要结合lifecycle使用。避免不必要的资源消耗。另外就是不能处理生产和消费速度不匹配的问题。
+ *
+ * SharedFlow可以看作一个加强版的生产，消费处理模型，可以设置多个应答者，可以设置缓存大小，处理生产大于消费的问题。当然也没有生命周期感应能力，需要结合Lifecycle的使用。
+ *
  */
 
 fun main() {
