46828131a21f82699d8137ac206d0c29c8785d0a
陈建祥 <853409335@qq.com>
2025-02-10 23:27:18 +0800

    feaq:测试feature module使用插件，自动生成文件

---
+++ featuretest/src/main/java/com/eric/feature/vault/VaultActivityViewModel.kt
@@ -0,0 +1,8 @@
+
+package com.eric.feature.vault
+
+import com.transsion.architecturemodule.base.viewmodel.BaseViewModel
+
+class VaultActivityViewModel : BaseViewModel() {
+
+}
---
+++ featuretest/src/main/java/com/eric/feature/vault/VaultFragmentViewModel.kt
@@ -0,0 +1,8 @@
+
+package com.eric.feature.vault
+
+import com.transsion.architecturemodule.base.viewmodel.BaseViewModel
+
+class VaultFragmentViewModel : BaseViewModel() {
+
+}
