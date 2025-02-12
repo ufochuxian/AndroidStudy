3a7e520d466a77286214b057bf5d53de41f4a741
陈建祥 <853409335@qq.com>
2025-02-12 14:59:55 +0800

    feaq:使用opencv完成图像的模糊以及灰度等功能

--- app/src/main/java/com/eric/androidstudy/applock/AppLockActivity.kt
+++ app/src/main/java/com/eric/androidstudy/applock/AppLockActivity.kt
@@ -8,6 +8,7 @@ import com.eric.androidstudy.databinding.ActivityApplockBinding
 import com.transsion.architecturemodule.base.activity.BaseVMActivity
 import org.opencv.android.Utils
 import org.opencv.core.Mat
+import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
 class AppLockActivity : BaseVMActivity<ActivityApplockBinding, AppLockActivityViewModel>() {
@@ -16,22 +17,23 @@ class AppLockActivity : BaseVMActivity<ActivityApplockBinding, AppLockActivityVi
     }
 
     override fun initView(savedInstanceState: Bundle?) {
-//        showPage(R.id.container, AppLockFragment.newInstance(), AppLockFragment.TAG, true)
-
         // 读取 drawable 图片并转换为 Mat
         val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img)
         val originalMat = Mat()
         Utils.bitmapToMat(bitmap, originalMat)
 
         // 转换为灰度图
-        val grayMat = applyGrayScale(originalMat)
+//        val grayMat = applyGrayScale(originalMat)
 
-        // 可选：如果要显示处理后的图片，需要转换回 Bitmap
-        val grayBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-        Utils.matToBitmap(grayMat, grayBitmap)
+        // 对灰度图进行模糊处理
+        val blurredMat = applyBlur(originalMat)
 
-        // 在 ImageView 显示灰度图
-        mBinding?.imageView?.setImageBitmap(grayBitmap)
+        // 将处理后的 Mat 转换回 Bitmap
+        val blurredBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(blurredMat, blurredBitmap)
+
+        // 在 ImageView 显示模糊后的图片
+        mBinding?.imageView?.setImageBitmap(blurredBitmap)
     }
 
     override fun initObserve() {}
@@ -42,9 +44,19 @@ class AppLockActivity : BaseVMActivity<ActivityApplockBinding, AppLockActivityVi
     /**
      * 处理灰度化
      */
-    fun applyGrayScale(src: Mat?): Mat {
+    private fun applyGrayScale(src: Mat?): Mat {
         val grayMat = Mat()
         Imgproc.cvtColor(src, grayMat, Imgproc.COLOR_BGR2GRAY)
         return grayMat
+    }
+
+    /**
+     * 处理模糊化
+     */
+    private fun applyBlur(src: Mat?): Mat {
+        val blurredMat = Mat()
+        // 使用高斯模糊，参数为：输入 Mat、输出 Mat、模糊核大小、SigmaX
+        Imgproc.GaussianBlur(src, blurredMat, Size(25.0, 25.0), 0.0)
+        return blurredMat
     }
 }
