3e082a7b65266cbd9fde439bba50d4ac686bd19f
陈建祥 <853409335@qq.com>
2025-03-12 19:59:39 +0800

    feaq:avif文件

--- app/src/main/java/com/eric/androidstudy/AvifActivity.kt
+++ app/src/main/java/com/eric/androidstudy/AvifActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import java.io.File
 
 class AvifActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,10 @@ class AvifActivity : AppCompatActivity() {
 
         val imageView: ImageView = findViewById(R.id.imageView)
 
+        // 读取应用私有 files 目录中的 AVIF 文件
+        val avifFile = File(filesDir, "kimono.crop.avif")
         Glide.with(this)
-            .load("file:///storage/emulated/0/Download/sample.avif")
+            .load(avifFile)
             .into(imageView)
     }
 }
